9970c7c4a755c865a6160fde166613bd85076e97
paulo.dsn.pack <paulo.dsn.pack@alterdata.com.br>
2024-01-25 01:21:00 -0300

    Commitando os deletes que eu havia esquecido.

--- src/main/resources/banco.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ingrediente
-(
-    nome_ingrediente VARCHAR(255) NOT NULL,
-    valor_ingrediente VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_pessoa)
-);
-
-INSERT INTO pessoa (nome_pessoa, email_pessoa) VALUES ('Paulo', 'teste@teste.com.br');
